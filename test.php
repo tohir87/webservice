a9e1ff6d4595cfca71b897515771521496dae494
Tohir Omoloye <thohiru.omoloye@talentbase.ng>
2015-07-16 18:23:04 +0100

    Updated services file

--- test.php
+++ test.php
@@ -1,14 +1,38 @@
 <?php 
-header("Content-type: application/json");
-$email = $_GET['email'];
-$id = $_GET['id'];
 
-// perform any operations you want to perform
-$result = ['email' => $email,
-	'id' => $id,
-	'first_name' => 'Jane',
-	'last_name' => 'Jone'] ;
+// process client url
+if ( !empty($_GET['id'])	){
+	$id = $_GET['id'];
+	
+	switch ($id){
+		case 1:
+		$first_name = 'Sade';
+		$last_name = 'Samuel';
+		$email = 'info@aol.com';
+		break;
+		case 2:
+		$first_name = 'Victor';
+		$last_name = 'Buari';
+		$email = 'info@yahoo.com';
+		break;
+		default:
+		$first_name = null;
+		$last_name = null;
+		$email = null;
+	}
 
-echo json_encode($result)
+	deliver_response($email, $first_name, $last_name);
+}
+
+function deliver_response($email, $first_name, $last_name){
+	header("HTTP/1.1 ");
+	$json_response = json_encode([ 'first_name' => $first_name,
+	'last_name' => $last_name,
+	'email' => $email]);
+	echo $json_response;
+}
+
+
+
 
 ?>
